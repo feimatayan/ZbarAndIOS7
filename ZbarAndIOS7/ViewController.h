71b71417c9b1af587ed49ce1099be8fbe52a8fe3
linghang <linghang@linghangdeMacBook-Pro.local>
2014-12-17 21:07:57 +0800

    Initial Commit

---
+++ ZbarAndIOS7/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ZbarAndIOS7
+//
+//  Created by linghang on 14-12-17.
+//  Copyright (c) 2014年 linghang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ZbarAndIOS7/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ZbarAndIOS7
+//
+//  Created by linghang on 14-12-17.
+//  Copyright (c) 2014年 linghang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
